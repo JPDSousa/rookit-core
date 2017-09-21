b1cb7d440bc214e0088ddb6f927bdbbfb208f26d
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-09-21 10:36:24 +0100

    ExtendedCLI

---
+++ src/main/java/org/rookit/runner/RookitShell.java
@@ -0,0 +1,15 @@
+package org.rookit.runner;
+
+import org.rookit.utils.log.Logs;
+import org.rookit.utils.log.Validator;
+
+@SuppressWarnings("javadoc")
+public class RookitShell {
+	
+	private static final Validator VALIDATOR = new Validator(Logs.CORE); 
+
+	public static void main(String[] args) {
+		
+	}
+
+}
