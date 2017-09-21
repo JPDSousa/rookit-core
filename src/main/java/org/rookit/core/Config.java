fe8e286f2bf6c6c2507d64e42f6851f5520d379b
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-09-21 10:36:51 +0100

    shell mode basics

---
+++ src/main/java/org/rookit/core/Config.java
@@ -0,0 +1,13 @@
+package org.rookit.core;
+
+public class Config {
+	
+	public static final Config read() {
+		return new Config();
+	}
+	
+	private Config() {
+		
+	}
+
+}
--- src/main/java/org/rookit/runner/RookitShell.java
+++ src/main/java/org/rookit/runner/RookitShell.java
@@ -1,15 +1,51 @@
 package org.rookit.runner;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.extendedCLI.command.CLIBuilder;
+import org.extendedCLI.command.ExtendedCLI;
+import org.extendedCLI.exceptions.NoSuchCommandException;
+import org.rookit.core.Config;
 import org.rookit.utils.log.Logs;
 import org.rookit.utils.log.Validator;
 
 @SuppressWarnings("javadoc")
 public class RookitShell {
 	
-	private static final Validator VALIDATOR = new Validator(Logs.CORE); 
+	private static final Validator VALIDATOR = new Validator(Logs.CORE);
 
-	public static void main(String[] args) {
+	private final ExtendedCLI cli;
+	
+	private RookitShell(Config configuration) {
+		final CLIBuilder builder = new CLIBuilder(false);
 		
+		cli = builder.build();
+	}
+	
+	private void execute(String input) throws NoSuchCommandException {
+		cli.execute(input);
+	}
+	
+	private void start(BufferedReader reader) throws IOException, NoSuchCommandException {
+		String line;
+		while((line = reader.readLine()) != null) {
+			execute(line);
+		}
+		reader.close();
+	}
+	
+	public static void main(String[] args) throws NoSuchCommandException, IOException {
+		final Config config = Config.read();
+		final RookitShell shell = new RookitShell(config);
+		
+		if(args.length > 0) {
+			shell.execute(String.join(" ", args));
+		}
+		else {
+			shell.start(new BufferedReader(new InputStreamReader(System.in)));
+		}
 	}
 
 }
