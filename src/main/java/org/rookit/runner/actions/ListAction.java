ef0d5b63fabf68a0a384e269869c9f33aecc7389
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-09-22 08:35:21 +0100

    basic list action

---
+++ src/main/java/org/rookit/runner/actions/ListAction.java
@@ -0,0 +1,26 @@
+package org.rookit.runner.actions;
+
+import org.extendedCLI.command.AbstractCommand;
+import org.extendedCLI.command.Command;
+import org.extendedCLI.command.ExtendedCommandLine;
+
+@SuppressWarnings("javadoc")
+public class ListAction extends AbstractCommand implements Command {
+
+	protected ListAction() {
+		super(ListOptions.createArguments());
+	}
+
+	@Override
+	protected void execute(ExtendedCommandLine arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void undo() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
