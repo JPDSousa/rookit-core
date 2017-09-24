3cc854a5ac590451fd9a814d9b0a2df8a4fb0e47
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-09-24 08:50:13 +0100

    refactored by TrackPath

--- src/main/java/org/rookit/core/utils/TrackPathNormalizer.java
+++ src/main/java/org/rookit/core/utils/TrackPathNormalizer.java
@@ -1,14 +1,8 @@
 package org.rookit.core.utils;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
-
 import org.rookit.parser.utils.TrackPath;
 
 import com.mpatric.mp3agic.Mp3File;
-import com.mpatric.mp3agic.NotSupportedException;
 
 @SuppressWarnings("javadoc")
 public class TrackPathNormalizer {
@@ -19,14 +13,12 @@ public class TrackPathNormalizer {
 		this.path = path;
 	}
 	
-	public void removeTags() throws NotSupportedException, IOException {
-		final String tempName = "aksjdhashdfasgdjasdadfdgf";
+	public void removeTags() {
 		final Mp3File mp3 = path.getMp3();
 		mp3.removeId3v1Tag();
 		mp3.removeId3v2Tag();
 		mp3.removeCustomTag();
-		mp3.save(tempName);
-		Files.move(Paths.get(tempName), path.getPath(), StandardCopyOption.REPLACE_EXISTING);
+		path.updateMP3(mp3);
 	}
 
 }
