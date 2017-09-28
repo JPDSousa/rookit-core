d38975c9a167c2ee8b3877a033406c1230ec024d
Joao Sousa <jpd.sousa@campus.fct.unl.pt>
2017-09-28 20:22:13 +0100

    defaults format lists and database connection

--- src/test/java/org/rookit/core/utils/TestResources.java
+++ src/test/java/org/rookit/core/utils/TestResources.java
@@ -6,6 +6,8 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.rookit.mongodb.DBManager;
+import org.rookit.parser.formatlist.FormatList;
 import org.rookit.parser.utils.TrackPath;
 import org.rookit.utils.resource.Resources;
 
@@ -13,6 +15,17 @@ import org.rookit.utils.resource.Resources;
 public class TestResources {
 	
 	public static final Path TRACKS = Resources.RESOURCES_TEST.resolve("tracks");
+	public static final Path FORMATS = Resources.RESOURCES_TEST.resolve("parser").resolve("formats.txt");
+
+	private static final DBManager DB = DBManager.open("localhost", 27020, "test");
+	
+	public static final DBManager getDBConnection() {
+		return DB;
+	}
+	
+	public static final FormatList getFormatList() throws IOException {
+		return FormatList.readFromPath(FORMATS);
+	}
 	
 	public static final List<TrackPath> getTracks() throws IOException {
 		return Files.list(TRACKS)
